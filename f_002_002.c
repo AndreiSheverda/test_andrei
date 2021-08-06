4946c39eb3bb2b5eab5a8569e755af4b72224b92
AndreiSheverda <andreisheverda@yandex.ru>
2021-08-06 11:58:00 +0300

    задачи 002_001 002_002 002_003 003_004

---
+++ f_002_002.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <math.h>  // для математических функций
+#define _USE_MATH_DEFINES   // директива - для числа Пи, число Пи 
+
+float f_002_002(float fR_m) {
+	
+	float result;
+	int GERC;
+	
+	result=M_PI*pow(fR_m, 2);  /* M_PI  - это число ПИ*/
+	
+	printf("\n===================");
+	printf("\n R (metr) =%f", fR_m);
+	printf("\n result =%f", result);
+	printf("\n=====================\n");
+	
+	for(GERC=700; GERC>=400; GERC=GERC-50) beep(GERC,120);
+	return result;
+}
