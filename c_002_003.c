b4feb407081d57805882cb47e2f9a6f7918c7cd5
AndreiSheverda <andreisheverda@yandex.ru>
2021-08-18 18:50:12 +0300

    задание 002_003

---
+++ c_002_003.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+float f_002_003(float R_sm);
+
+int main()
+{
+	float R_sm;
+		
+	
+	R_sm=100.0;
+	f_002_003(R_sm);
+	
+	
+	return 0;
+  
+}
---
+++ f_002_003.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <math.h>  // для математических функций
+#define _USE_MATH_DEFINES   // директива - для числа Пи, число Пи 
+
+float f_002_003(float R_sm) {
+	
+	float result;
+		
+	
+	result=M_PI*pow(R_sm/100.0, 2);  /* M_PI  - это число ПИ*/
+	
+	printf("\n===================");
+	printf("\n R (sm) = %f", R_sm);
+	printf("\n result = %f", result);
+	printf("\n=====================\n");
+	
+	
+	return result;
+}
